7df5ef1e0cb1489fe9345b41d7d21340e9d483af
sabrinaaquino <sabrina.aquino@hotmail.com>
2023-03-23 03:44:19 -0300

    added some styles

--- main.js
+++ main.js
@@ -3,15 +3,22 @@ const apiKey = 'fdb9776aeaf5451d9f622810232303';
 async function fetchWeatherData(cityName){
     const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}`;
     const response = await fetch(apiUrl);
+    
+    if (!response.ok) {
+        throw new Error('Failed to fetch data');
+    }
+    
     const data = await response.json();
     return data;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const getWeatherDataButton = document.getElementById('getWeatherData');
+    const getWeatherDataButton = document.getElementById('weatherForm');
     
     if (getWeatherDataButton) {
-        getWeatherDataButton.addEventListener('click', () => {
+        getWeatherDataButton.addEventListener('submit', (event) => {
+            event.preventDefault();
+
             (async () => {
                 const cityName = document.getElementById('cityInput').value;
                 const weatherDataElement = document.getElementById('weatherData');
@@ -25,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     weatherDataElement.innerHTML = `<h2>${cityName}</h2><p>Temperature: ${temperature}°C</p><p>Condition: ${condition}</p>`;
                 } catch (error) {
                     console.error("Error", error);
-                    weatherDataElement.innerHTML = `<p>Error: Invalid city name or unable to fetch data. Please try again.</p>`;
+                    weatherDataElement.innerHTML = `<p class="error-message">Error: Invalid city name or unable to fetch data. Please try again.</p>`;
                 }
             })();
       });
