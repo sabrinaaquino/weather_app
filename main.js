a9d506ed533bea7bb6f5c5ba1247d2bca529942e
sabrinaaquino <sabrina.aquino@hotmail.com>
2023-03-23 15:29:00 -0300

    reverting changes

--- main.js
+++ main.js
@@ -3,13 +3,22 @@ const apiKey = 'fdb9776aeaf5451d9f622810232303';
 async function fetchWeatherData(cityName){
     const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}`;
     const response = await fetch(apiUrl);
-    
+
     if (!response.ok) {
         throw new Error('Failed to fetch data');
     }
-    
+
     const data = await response.json();
     return data;
+}
+
+function updateBackgroundVideo(temperature) {
+    const videoElement = document.getElementById('bgVideo');
+    const sourceElement = videoElement.getElementsByTagName('source')[0];
+    const videoName = temperature > 25 ? 'sunny' : 'partlycloudy';
+    sourceElement.src = `${videoName}.mp4`;
+    videoElement.load();
+    videoElement.play();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -23,17 +32,20 @@ document.addEventListener('DOMContentLoaded', () => {
                 const cityName = document.getElementById('cityInput').value;
                 const weatherDataElement = document.getElementById('weatherData');
                     try {
-                    const weatherData = await fetchWeatherData(cityName);
+                    const weatherData= await fetchWeatherData(cityName);
 
                     const temperature = weatherData.current.temp_c;
                     const condition = weatherData.current.condition.text;
+                    
+                    updateBackgroundVideo(weatherData.current.temp_c);
 
+                   
                     //Displaying in the page
                     weatherDataElement.innerHTML = `<h2>${cityName}</h2><p>Temperature: ${temperature}°C</p><p>Condition: ${condition}</p>`;
                 } catch (error) {
                     console.error("Error", error);
                     weatherDataElement.innerHTML = `<p class="error-message">Error: Invalid city name or unable to fetch data. Please try again.</p>`;
-                }
+                }   
             })();
       });
     }
